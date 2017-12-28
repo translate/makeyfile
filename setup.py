ee2c437499ed7e304a6bf86c692c12843f0f043f
Ryan Northey <ryan@synca.io>
2017-12-28 21:39:44 +0000

    Add boilerplate setup

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python makeyfile
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+
+install_requires = []
+extras_require = {}
+extras_require['test'] = [
+    "pytest",
+    "pytest-mock",
+    "requests_mock",
+    "coverage",
+    "pytest-coverage",
+    "codecov",
+    "flake8"],
+
+setup(
+    name='makeyfile',
+    version='0.0.1',
+    description='Python Makeyfile',
+    long_description="Python makeyfile",
+    url='https://github.com/phlax/makeyfile',
+    author='Ryan Northey',
+    author_email='ryan@synca.io',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        ('License :: OSI Approved :: '
+         'GNU General Public License v3 or later (GPLv3+)'),
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='python make',
+    install_requires=install_requires,
+    extras_require=extras_require,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    entry_points={
+        'console_scripts': [
+            'makey = makeyfile.runner:main',
+        ],
+    },
+    include_package_data=True)

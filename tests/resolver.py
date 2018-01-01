91781714d98dd4bc497c0125bf735be6317c65e7
Ryan Northey <ryan@synca.io>
2018-01-01 14:15:45 +0000

    Update resolver tests

--- tests/resolver.py
+++ tests/resolver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
+
 import pytest
 
 from mock import MagicMock, PropertyMock, patch
@@ -21,32 +23,25 @@ def test_resolver_defaults():
     assert resolver.makey == [23]
 
 
-def test_resolver_resolve_registered():
+def test_resolver_commands():
     makey_mock = MagicMock()
-    runner_mock = MagicMock()
-    runner_mock.resolve = MagicMock(return_value=23)
-
-    makey_p = PropertyMock(return_value=dict(foo=dict(bar="baz")))
+    makey_p = PropertyMock(
+        return_value=OrderedDict(
+            (("foo0", OrderedDict((("bar0a", "baz0a"), ("bar0b", "baz0b")))),
+             ("foo1", OrderedDict((("bar1a", "baz1a"), ("bar1b", "baz1b")))))))
     type(makey_mock).makey = makey_p
-
-    runners_p = PropertyMock(return_value=dict(foo=runner_mock))
+    runners_p = PropertyMock(
+        return_value=dict(foo0="x", foo1="y"))
     type(makey_mock).runners = runners_p
-
     resolver = Resolver(makey_mock)
-
-    with pytest.raises(UnrecognizedMakeyError):
-        resolver.resolve_registered('NOT', 'EXISTS')
-    assert not runners_p.called
-
-    with pytest.raises(UnrecognizedMakeyError):
-        resolver.resolve_registered('foo', 'ALSO NOT EXISTS')
-    assert not runners_p.called
-
-    result = resolver.resolve_registered("foo", "bar")
+    commands = resolver.commands
     assert runners_p.called
-    assert runner_mock.resolve.called
-    assert runner_mock.resolve.call_args[0][0] == "baz"
-    assert result == ('foo', 23)
+    assert (
+        commands.items()
+        == [('bar0a', 'foo0'),
+            ('bar0b', 'foo0'),
+            ('bar1a', 'foo1'),
+            ('bar1b', 'foo1')])
 
 
 def test_resolver_resolve_command():
@@ -61,13 +56,17 @@ def test_resolver_resolve_command():
     type(makey_mock).runners = runners_p
 
     resolver = Resolver(makey_mock)
-    with patch('makeyfile.resolver.Resolver.resolve_registered') as m:
-        m.side_effect = UnrecognizedMakeyError
+    _patch = patch(
+        'makeyfile.resolver.Resolver.commands',
+        new_callable=PropertyMock)
+
+    with _patch as m:
+        m.return_value = dict(bar="foo")
         with pytest.raises(UnrecognizedMakeyError):
             resolver.resolve('DOES NOT EXIST')
-        assert m.call_args[0] == ('foo', 'DOES NOT EXIST')
-    assert makey_p.called
+        assert m.called
 
-    with patch('makeyfile.resolver.Resolver.resolve_registered') as m:
-        resolver.resolve('foo')
-        assert m.call_args[0] == ('foo', 'foo')
+    with _patch as m:
+        m.return_value = dict(bar="foo")
+        result = resolver.resolve('bar')
+        assert result == ('foo', 'baz')

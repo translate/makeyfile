b3db0a4ad5686519d66e4f56a3e74dbec91f9abb
Ryan Northey <ryan@synca.io>
2018-01-01 17:50:37 +0000

    Add command loading to makeyfile

--- makeyfile/makeyfile.py
+++ makeyfile/makeyfile.py
@@ -1,4 +1,6 @@
 
+import os
+
 from . import registry
 from .discovery import Discovery
 from .loader import Loader
@@ -7,6 +9,7 @@ from .runners import Runners
 
 
 class Makeyfile(object):
+    command_filename = "makeyfile.commands.json"
 
     def __init__(self):
         self.registry = registry
@@ -19,3 +22,20 @@ class Makeyfile(object):
     @property
     def filepath(self):
         return self.discovery.find()
+
+    @property
+    def command_filepath(self):
+        return os.path.join(
+            os.path.dirname(__file__),
+            self.command_filename)
+
+    def load_commands(self):
+        return self.loader.load(self.command_filepath)
+
+    def load_makeyfile(self):
+        return self.loader.load(self.filepath)
+
+    def load(self):
+        makey = self.load_commands()
+        makey.update(self.load_makeyfile())
+        return makey

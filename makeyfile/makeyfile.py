dac3d59ae3b3e130f704a15f2c9a9784f5298a0a
Ryan Northey <ryan@synca.io>
2018-01-01 12:59:36 +0000

    Add Runners class to instantiate runners

--- makeyfile/makeyfile.py
+++ makeyfile/makeyfile.py
@@ -3,12 +3,14 @@ from . import registry
 from .discovery import Discovery
 from .loader import Loader
 from .resolver import Resolver
+from .runners import Runners
 
 
 class Makeyfile(object):
 
     def __init__(self):
         self.registry = registry
+        self.runners = Runners(self)
         self.discovery = Discovery()
         self.loader = Loader()
         self.filepath = self.discovery.find()
--- makeyfile/runners/__init__.py
+++ makeyfile/runners/__init__.py
@@ -1,10 +1,45 @@
 
+from collections import OrderedDict
+
 from .py import PythonRunner
 from .sequence import SequenceRunner
 from .shell import ShellRunner
 
 
 def register_defaults(registry):
-    registry.register("runner", "sequence", SequenceRunner())
-    registry.register("runner", "python", PythonRunner())
-    registry.register("runner", "shell", ShellRunner())
+    registry.register("runner", "sequence", SequenceRunner)
+    registry.register("runner", "python", PythonRunner)
+    registry.register("runner", "shell", ShellRunner)
+
+
+class Runners(object):
+
+    def __init__(self, makeyfile):
+        self._runners = OrderedDict()
+        self.makeyfile = makeyfile
+        self.load_runners()
+
+    def load_runners(self):
+        for name, runner in self.makeyfile.registry["runner"].items():
+            self[name] = runner(self.makeyfile)
+
+    def __getitem__(self, k):
+        return self._runners[k]
+
+    def __setitem__(self, k, v):
+        self._runners[k] = v
+
+    def __contains__(self, k):
+        return k in self._runners
+
+    def __iter__(self):
+        return iter(self._runners.keys())
+
+    def keys(self):
+        return self._runners.keys()
+
+    def items(self):
+        return self._runners.items()
+
+    def values(self):
+        return self._runners.values()
